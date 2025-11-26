5c7b7a438c0ae28fee7f262508b4ca3d7d32ccad
Nawaf <Alzhraniii999@gmail.com>
2025-11-26 17:32:13 +0000

    Print substring

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-
+MiddleWord = word[1:8]
 print(f"First 3 letters: {word[0:3]}")
 print(f"Last 2 letters: {word[7:9]}")
-print(f"Middle word: {word[1:8]}")
+print(f"Middle word: {MiddleWord}")
