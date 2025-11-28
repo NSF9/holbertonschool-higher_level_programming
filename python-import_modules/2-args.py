97eaa523b8b2781eba363c25193bd6de124c2ff4
Nawaf <Alzhraniii999@gmail.com>
2025-11-28 16:47:25 +0000

    print arguments count

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
+if __name__ == "__main__":
     argv = sys.argv[1:]
     argc = len(argv)
 
     if argc == 0:
         print("0 arguments.")
     if argc == 1:
-        print(f"1 argument:\n1:{argc[0]}")
+        print(f"1 argument:\n1: {argv[0]}")
     if argc > 1:
-        print(f"{argc} argument:")
+        print(f"{argc} arguments:")
         for i, arg in enumerate(argv):
             print(f"{i + 1}: {arg}")
