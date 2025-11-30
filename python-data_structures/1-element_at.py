96789576675221e5ce6b1d4c093ef5bd515a5aa5
Nawaf <Alzhraniii999@gmail.com>
2025-11-30 12:33:23 +0000

    Check if idx is zero or out of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+        Length = len(my_list) - 1
 
-        if idx < 0 or idx > len(my_list):
+        if idx > Length:
+                return None
+        if idx < 0:
             return None
         else:
              return my_list[idx]
+
