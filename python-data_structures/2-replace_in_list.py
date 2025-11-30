82122b91e86f4b0c943b1f2f266b3351b02efe7d
Nawaf <Alzhraniii999@gmail.com>
2025-11-30 17:44:56 +0000

    Replace a value in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    
+
     NewValue = element
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         my_list[idx] = NewValue
-        return my_list 
+        return my_list
