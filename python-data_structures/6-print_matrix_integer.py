7ecdd2db0990bb0b0c7f2317d25f9fd4c27aa69e
Nawaf <Alzhraniii999@gmail.com>
2025-12-02 11:05:54 +0000

    Print two dimontioal Array

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join(str(row) for row in row), end="$")
+        for i in range(len(row)):
+            if i != len(row) - 1:
+                print("{}".format((row[i])),end = " ")
+            else:
+                  print("{:d}".format((row[i])), end="")
         print()
