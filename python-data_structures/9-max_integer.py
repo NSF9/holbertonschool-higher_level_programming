ef08391a1f938d617f248d6988b2cf77c1cefd95
Nawaf <Alzhraniii999@gmail.com>
2025-12-02 19:01:05 +0000

    Print max number in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    Max_value = my_list[0]
-    if my_list == 0:
+
+    if my_list == []:
         return None
-    
+
+    Max_value = my_list[0]
+
     for i in my_list:
-        if i > Max_value: 
+        if i > Max_value:
             Max_value = i
 
     return Max_value
