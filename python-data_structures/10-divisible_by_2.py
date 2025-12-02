2e8e71903e249da3ba6d78166b6337c6f5ded674
Nawaf <Alzhraniii999@gmail.com>
2025-12-02 19:30:30 +0000

    print Odd numbers

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
 
     for i in range(len(my_list)):
-        if i % 2 != 0:
+        if my_list[i] % 2 == 0:      # ← هنا الفحص الصحيح
             new_list.append(True)
         else:
             new_list.append(False)
