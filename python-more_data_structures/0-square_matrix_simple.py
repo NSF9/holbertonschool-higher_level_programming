79b02b21d35aabe34a2b31522f33da4a2f1a93d6
Nawaf <Alzhraniii999@gmail.com>
2025-12-03 13:57:45 +0000

    print square of matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+     return matrix
+
+    new_list = []  
+    for row in matrix:
+        new_row = []
+        for number in row:
+            new_row.append(number ** 2)
+        new_list.append(new_row)
+
+    return new_list
