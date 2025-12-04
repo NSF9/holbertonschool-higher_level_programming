0aec33285e213bb2746da7f512602c487dfec2ad
Nawaf <Alzhraniii999@gmail.com>
2025-12-04 13:33:39 +0000

    mapping method

---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    def multiply(x):
+        return x * number
+    new_list = my_list.copy()
+    return list(map(multiply, my_list))
