d2ae7188d20042096d5f82c6e20c1df14cc45528
Nawaf <Alzhraniii999@gmail.com>
2025-12-04 12:25:20 +0000

    Replcae a value in a Dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not key in a_dictionary[key]:
-        a_dictionary["city"] = value
+    a_dictionary[value] = value
     return a_dictionary
