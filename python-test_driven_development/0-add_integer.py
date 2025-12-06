36e7bf4b3c3f454c9d22672436494e7742a850ee
Nawaf <Alzhraniii999@gmail.com>
2025-12-06 16:56:31 +0000

    test add function and docstrings

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,16 +5,17 @@ It ensures both inputs are integers or floats (cast to int), and raises
 TypeError or OverflowError where appropriate.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats casted to integers.
 
     Args:
-        a: First number (int or float).
-        b: Second number (int or float, default is 98).
+        a (int or float): First number.
+        b (int or float, optional): Second number. Defaults to 98.
 
     Returns:
-        The integer sum of a and b.
+        int: The sum of a and b after casting to integers.
 
     Raises:
         TypeError: If a or b are not int or float.
@@ -22,10 +23,14 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     if a == float('inf') or a == float('-inf'):
         raise OverflowError()
+
     if b == float('inf') or b == float('-inf'):
         raise OverflowError()
+
     return int(a) + int(b)
