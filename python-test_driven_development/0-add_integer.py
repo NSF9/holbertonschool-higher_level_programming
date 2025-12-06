cb3f6e21c6b5b48bd6663a36d7170b688795911d
Nawaf <Alzhraniii999@gmail.com>
2025-12-06 16:47:12 +0000

    test add function and docstrings

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """
 This module defines the add_integer function that adds two numbers.
 It ensures both inputs are integers or floats (cast to int), and raises
@@ -7,7 +6,20 @@ TypeError or OverflowError where appropriate.
 """
 
 def add_integer(a, b=98):
+"""
+    Adds two integers or floats casted to integers.
 
+    Args:
+        a: First number (int or float).
+        b: Second number (int or float, default is 98).
+
+    Returns:
+        The integer sum of a and b.
+
+    Raises:
+        TypeError: If a or b are not int or float.
+        OverflowError: If a or b are infinity.
+    """
     if not isinstance(a,(int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
