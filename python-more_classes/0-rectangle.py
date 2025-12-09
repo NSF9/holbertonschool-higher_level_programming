27868c5d0a77d7e21069a3f12fab08c593f0630e
Nawaf <Alzhraniii999@gmail.com>
2025-12-09 12:04:05 +0000

    Add empty Rectangle class with basic docstrings

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: rectangle
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    Currently, this class does not contain any attributes or methods.
+    It serves as a placeholder for future rectangle-related logic.
+    """
+
+    pass
