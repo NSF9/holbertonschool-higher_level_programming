0af3d4d5ad15d80bcfe88e4c82062480c7802868
Nawaf <Alzhraniii999@gmail.com>
2025-12-19 11:27:17 +0000

    Implement BaseGeometry class with abstract area() method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that defines the BaseGeometry class with a placeholder method for area.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry operations. Intended to be subclassed.
+    """
+
+    def __init__(self):
+        """
+        Initializes the BaseGeometry class. Currently does nothing.
+        """
+        pass
+
+    def area(self):
+        """
+        Raises an exception indicating that the area method is not implemented.
+        Should be overridden by subclasses.
+        """
+        raise NotImplementedError("area() is not implemented")
