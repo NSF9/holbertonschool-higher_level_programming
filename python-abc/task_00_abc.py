6d6f9e80fbd51fb15817c51dc12a99fad134dc89
Nawaf <Alzhraniii999@gmail.com>
2025-12-20 18:27:57 +0000

    Add abstract Animal class with Dog and Cat implementations

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,44 @@
-empty
+#!/usr/bin/python3
+"""
+This module defines an abstract Animal class and its concrete subclasses.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract base class representing an animal.
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method that should be implemented by all subclasses to return
+        the sound the animal makes.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Concrete class representing a dog, subclass of Animal.
+    """
+
+    def sound(self):
+        """
+        Returns the sound that a dog makes.
+        """
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Concrete class representing a cat, subclass of Animal.
+    """
+
+    def sound(self):
+        """
+        Returns the sound that a cat makes.
+        """
+        return "Meow"
