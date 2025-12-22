c3514cdc5b7a51023b027c9af02af8407edce40e
Nawaf <Alzhraniii999@gmail.com>
2025-12-22 09:34:38 +0000

    Created READ me fiile and Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Module that defines a function to read a file and print its contents."""
 
+
 def read_file(filename=""):
     """Reads and prints the contents of a text file."""
-    with open(filename) as File:
-        info = File.read()
+    with open(filename) as file:
+        info = file.read()
         print(info, end="")
