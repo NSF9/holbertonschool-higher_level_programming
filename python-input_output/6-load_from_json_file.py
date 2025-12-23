8df1aa5107b43cae087ba5933c8dd3b91ece2275
Nawaf <Alzhraniii999@gmail.com>
2025-12-23 10:58:22 +0000

    Add function to load data from JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """
+    Load data from a JSON file and return the corresponding Python object.
+
+    Args:
+        filename (str): The path to the JSON file.
+
+    Returns:
+        object: The Python object resulting from parsing the JSON content.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
+
