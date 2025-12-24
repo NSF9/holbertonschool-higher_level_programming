6cad270d4da8703f810466352a40d8b1366ef950
Nawaf <Alzhraniii999@gmail.com>
2025-12-24 10:24:32 +0000

    Return object attributes as a dictionary using __dict__

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Converts an object to a dictionary for JSON serialization.
+"""
+
+def class_to_json(obj):
+    """
+    Get object's attributes as a dictionary.
+    """
+    return obj.__dict__
