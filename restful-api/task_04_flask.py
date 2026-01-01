c204208e5552d7df88fa6c6b153c1e66ee088dfe
Nawaf <Alzhraniii999@gmail.com>
2026-01-01 20:09:37 +0000

    Edit

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request
 
 App = Flask(__name__)
 
-users = {}
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"},
+    "alice": {"username": "alice", "name": "Alice", "age": 25, "city": "San Francisco"}
+}
 
 @App.route("/")
 def home():
-    return "Welcome to the Flask API"
-
+    return jsonify({"message": "Welcome to the Flask API"})
 
 @App.route("/data")
 def get_username():
@@ -21,31 +23,24 @@ def get_user(username):
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "user not found"}), 404
+        return jsonify({"error": "User not found"}), 404
 
 @App.route("/status")
 def status():
     return "OK"
 
-@App.route("/add_user", methods = ["POST"])
+@App.route("/add_user", methods=["POST"])
 def add_user():
-    try:
-        data = request.get_json()
-    except:
-        return jsonify({"error": "Invalid JSON"}), 400
-    
-    if not data or "username" not in data:
-        return jsonify({"error" : "username is required"}), 400 
-    
-    username = data ["username"]
+    data = request.get_json(silent=True)
+    if not data or not isinstance(data, dict) or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
 
+    username = data["username"]
     if username in users:
-        return jsonify({"error" : "username already exists"}), 409 
-    
+        return jsonify({"error": "Username already exists"}), 409
+
     users[username] = data
-    return jsonify({"message" : "User added", "user" : data}), 201
-
-
+    return jsonify({"message": "User added", "user": data}), 201
 
 if __name__ == "__main__":
     App.run()
