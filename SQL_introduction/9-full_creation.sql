8d81d188b25e340fa6e1e733d167d7276ab4804d
Nawaf <Alzhraniii999@gmail.com>
2026-01-13 18:25:35 +0300

    update file

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,6 @@
 -- check if database exist and create second_table
-CREATE second_table IF NOT EXISTS second_table(id, name, score)
+CREATE TABLE IF NOT EXISTS second_table(id, name, score)
+
+-- insert into second_table
 insert into second_table(id, name, score)
 values(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8)
