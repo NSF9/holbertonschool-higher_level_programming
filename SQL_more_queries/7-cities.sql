38f8317a1d92fa7f01e4539e178ea77e23152893
Nawaf <Alzhraniii999@gmail.com>
2026-01-15 22:51:37 +0300

    new update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS cities
 (
 
 	id INT AUTO_INCREMENT,
-	FOREIGN KEY (state_id)
-	REFERENCES states(id),
+	state_id int NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES state(state_id),
 	name VARCHAR(256) NOT NULL
 );
