51ebe9ecc79ee3863a671b6968dffd67dc4e94c0
Nawaf <Alzhraniii999@gmail.com>
2026-01-15 23:04:01 +0300

    update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- uses hbtn_usa database and creates cities table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS cities;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT PRIMARY KEY AUTO_INCREMENT,
