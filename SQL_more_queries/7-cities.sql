ba3a1d7c0035fac1f0dfb8c94cc800e5d0bc66d2
Nawaf <Alzhraniii999@gmail.com>
2026-01-15 22:59:33 +0300

    update

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,7 @@
 -- uses hbtn_usa database and creates cities table
-CREATE TABLE IF NOT EXISTS cities
-(
-
-	id INT AUTO_INCREMENT,
-	state_id int NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES state(state_id),
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    state_id INT,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
