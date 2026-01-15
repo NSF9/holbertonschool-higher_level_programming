4fe1fbd873e0c33ab1f4ae490303c6146da0679f
Nawaf <Alzhraniii999@gmail.com>
2026-01-15 22:57:33 +0300

    update

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,11 +1,9 @@
 -- create new database and create a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
-(
-
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL
-
-
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    state_id INT,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
