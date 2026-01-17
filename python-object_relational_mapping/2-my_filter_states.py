6794a079443774f676e9a74b868beca10e23f882
Nawaf <Alzhraniii999@gmail.com>
2026-01-17 22:48:01 +0300

    added parametrs query

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Filter states by name input (exact match, case-sensitive)"""
+
+import MySQLdb
+import sys
+
+
+def filter_states_by_name(username, password, database_name, state_name):
+    """Displays states with names matching the user input"""
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
+
+        cursor = db.cursor()
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        cursor.execute(query)
+
+        for row in cursor.fetchall():
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    filter_states_by_name(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
