1a830a71124b7fd1e941fbc948266dd518fd69ca
Nawaf <Alzhraniii999@gmail.com>
2026-01-17 20:53:50 +0300

    update

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- show list of tv genre name and count of the number of shows by inner join query 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows 
-FROM tv_show_genres
-JOIN tv_genres ON Tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
