869108514eb857a211bc7d8106a314fff4c27fc2
Nawaf <Alzhraniii999@gmail.com>
2026-01-30 15:38:26 +0300

    fix: adjust indentation and ensure consistent formatting in 5-to_integer.js and 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const strs = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 strs.forEach(str => {
-    console.log(str);
+  console.log(str);
 });
