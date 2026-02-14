aa8bf44664eb8ca6fa5e16f2d1d10c888d40d587
Nawaf <Alzhraniii999@gmail.com>
2026-02-14 16:49:59 +0300

    feat: add answers for questions 16 to 18 and enhance test script with additional functions

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -54,3 +54,25 @@ l1 = [1, 2, 3]
 l2 = l1
 l1 = l1 + [4]
 print(l2)
+
+def increment(n):
+    n += 1
+
+a = 1
+increment(a)
+print(a)
+
+def increment(n):
+    n.append(4)
+
+l = [1, 2, 3]
+increment(l)
+print(l)
+
+def assign_value(n, v):
+    n = v
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+assign_value(l1, l2)
+print(l1)
