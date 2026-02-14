97b6f9f7394cb36612169354730c8d1707197a65
Nawaf <Alzhraniii999@gmail.com>
2026-02-14 16:44:08 +0300

    feat: update answer files for questions 13 to 15 and enhance test script for identity comparison

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -40,3 +40,17 @@ print(l1 is l2)
 l1 = [1, 2, 3]
 l2 = l1
 print(l1 == l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+print(l1 is l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+l1.append(4)
+print(l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+l1 = l1 + [4]
+print(l2)
