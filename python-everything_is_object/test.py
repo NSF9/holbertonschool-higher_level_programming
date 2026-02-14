8ee1dabcca69b731cfc037591c7c3b9ba15ceb73
Nawaf <Alzhraniii999@gmail.com>
2026-02-14 16:38:30 +0300

    feat: add answer files for questions 8 to 29 and update test script for identity comparison

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -19,3 +19,19 @@ print(id(a), id(b))
 s1="best School"
 s2= s1
 print(s1 is s2)
+
+print(s1 is s2)
+
+
+s1 = "Best School"
+s2 = "Best School"
+print(s1 is s2)
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3] 
+print(l1 == l2)
+
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3] 
+print(l1 is l2)
